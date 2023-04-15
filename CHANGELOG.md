eac1b90567c75de3d52461da209226e5a7aca8cb
Roman Heinrich <roman.heinrich@gmail.com>
2023-04-15 15:13:32 +0200

    Prepare release 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## v0.1.1 (2023-04-15)
+
+- coloured logs
+- matching on copy-pasted `file:line` strings from ExUnit exceptions, like:
+  `lib/users/user_test.exs:54`
+- unit tests
+- Github CI
+
 ## v0.1.0 (2023-04-10)
 
 ### First release
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule TestIex.MixProject do
   use Mix.Project
 
   @github_url "https://github.com/mindreframer/maxo_test_iex"
-  @version "0.1.0"
+  @version "0.1.1"
   @description "Run ExUnit tests from IEx shell."
 
   def project do
