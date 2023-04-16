d2318368defac445c25647cb7e3aac983770e6f8
Roman Heinrich <roman.heinrich@gmail.com>
2023-04-16 17:38:22 +0200

    Chore: docs for TestIex.Config module

--- lib/test_iex/config.ex
+++ lib/test_iex/config.ex
@@ -1,14 +1,25 @@
 defmodule TestIex.Config do
+  @moduledoc """
+  Central module for all provided configuration options
+  """
+
+  @doc false
   def core_module(), do: Application.get_env(:maxo_test_iex, :core, TestIex.Core)
 
   @doc """
   FS events deduplication frame, so that we do not trigger too many test runs on file changes.
+
+  defaults:
+    `500`
   """
   def watcher_dedup_timeout(),
-    do: Application.get_env(:maxo_test_iex, :watcher_dedup_timeout, 2000)
+    do: Application.get_env(:maxo_test_iex, :watcher_dedup_timeout, 500)
 
   @doc """
   Should the watcher be started?
+
+  defaults:
+    `true`
   """
   def watcher_enable(),
     do: Application.get_env(:maxo_test_iex, :watcher_enable, true)
@@ -16,6 +27,9 @@ defmodule TestIex.Config do
   @doc """
   Watcher args, that are passed directly to `:file_system` package.
   - https://hexdocs.pm/file_system/readme.html
+
+  defaults:
+    - `[dirs: ["lib/", "test/"], latency: 0]`
   """
   def watcher_args(),
     do: Application.get_env(:maxo_test_iex, :watcher_args, dirs: ["lib/", "test/"], latency: 0)
