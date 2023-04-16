b5e36702047f486c2a2a3e59edc3b2d4770f0ad5
Roman Heinrich <roman.heinrich@gmail.com>
2023-04-16 17:03:00 +0200

    Feat: configure CI to not start TestIex.Watcher process

---
+++ config/runtime.exs
@@ -0,0 +1,8 @@
+import Config
+
+if config_env() == :test do
+  # NO need to watch for tests on CI
+  if System.get_env("CI"), do: config(:maxo_test_iex, watcher_enable: false)
+  config :maxo_test_iex, watcher_dedup_timeout: 500
+  config :maxo_test_iex, watcher_args: [dirs: ["lib/", "test/"], latency: 0]
+end
